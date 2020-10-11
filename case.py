bd149055a78baf39a70c97aff5ff805206987e4d
LeDevin <stenis34@gmail.com>
2020-10-11 16:56:24 +0200

    case set up

---
+++ case.py
@@ -0,0 +1,10 @@
+class case():
+    boat = False
+    shoted = False
+    def __init__(self):
+
+    def shot(self):
+        self.shoted=True
+    
+    def bindBoat(self,boat):
+        self.boat = boat
