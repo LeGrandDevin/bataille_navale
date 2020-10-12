c073ab3c7fcf8f831d80c3ddcf0bea49c8e1406a
LeDevin <stenis34@gmail.com>
2020-10-12 12:38:00 +0200

    update case and player
    an update

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
-import case
+from case import case
 class Player:
     team = []
     desk =[]
 
     def __init__(self,size_desk):
-        for i in range size_desk:
-            self.desk.add(new case)
+        for i in range(size_desk):
+            self.desk.append(case())
