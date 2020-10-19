5c01f16afae800f806aa4f52e0690df52399eddd
Damien AMALOU <damien.amalou@gmail.com>
2020-10-19 23:45:00 +0200

    grille mouseover...

--- testpygame.py
+++ testpygame.py
@@ -1,30 +1,40 @@
 import sys
 import pygameclass.casegraphic
+import pygameclass.Grillegraphic
 
+import math
 import pygame
 from pygame.locals import *
 
 pygame.init()
 
 # load
-fps = 60
+fps = 180
 fpsClock = pygame.time.Clock()
 
 width, height = 1280, 720
 screen = pygame.display.set_mode((width, height))
 # Variables
-casetest = pygameclass.casegraphic.Casegraphic(200, 200, 50, screen)
+casetest = pygameclass.Grillegraphic.grillegraphic(screen)
 
 # Game loop.
 while True:
     screen.fill((4, 129, 220))
-    casetest.drawcase()
+    casetest.drawgrid()
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+    x, y = pygame.mouse.get_pos()
+    gx, gy = casetest.coordScreenToGrid(x, y)
+
+    if pygame.mouse.get_pressed()[0]:
+        print(x, y)
+        print(gx, gy)
+
+    casetest.gridover(gx, gy)
     # Update.
 
     # Draw.
