d1089641398b9468389a7cde757ffe73bfac8f0a
LeDevin <stenis34@gmail.com>
2020-10-20 23:45:19 +0200

    debug grille

--- testpygame.py
+++ testpygame.py
@@ -1,9 +1,9 @@
+import pygame
 import sys
 import pygameclass.casegraphic
 import pygameclass.Grillegraphic
 
 import math
-import pygame
 from pygame.locals import *
 
 pygame.init()
@@ -14,14 +14,19 @@ fpsClock = pygame.time.Clock()
 
 width, height = 1280, 720
 screen = pygame.display.set_mode((width, height))
+
+
+
 # Variables
-casetest = pygameclass.Grillegraphic.grillegraphic(screen)
+
+casetest = pygameclass.Grillegraphic.grillegraphic(screen,50 ,50)
+casetest2 = pygameclass.Grillegraphic.grillegraphic(screen,700 ,50)
 
 # Game loop.
 while True:
     screen.fill((4, 129, 220))
     casetest.drawgrid()
-
+    casetest2.drawgrid()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -29,12 +34,24 @@ while True:
 
     x, y = pygame.mouse.get_pos()
     gx, gy = casetest.coordScreenToGrid(x, y)
+    gx2, gy2 = casetest2.coordScreenToGrid(x, y)
 
     if pygame.mouse.get_pressed()[0]:
         print(x, y)
-        print(gx, gy)
+        print('le 1 :::' , gx, gy)
+        print('le 2 :::' , gx2, gy2)
+        print('last coord' , casetest2.lastcoord)
 
-    casetest.gridover(gx, gy)
+    if casetest.inGrid(x,y):
+        casetest.gridover(gx, gy)
+    else:
+        casetest.gridout(casetest.lastcoord[0],casetest.lastcoord[1])
+
+    if casetest2.inGrid(x,y):
+        casetest2.gridover(gx2, gy2)
+    else:
+        casetest2.gridout(casetest2.lastcoord[0],casetest2.lastcoord[1])
+
     # Update.
 
     # Draw.
