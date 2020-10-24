35902a033dc97a731bb2725128ad03d904cfbd3d
Damien AMALOU <damien.amalou@gmail.com>
2020-10-24 23:44:44 +0200

    click sur image bateau sprites collidepoint

--- testpygame.py
+++ testpygame.py
@@ -24,14 +24,27 @@ g = True
 
 # Variables
 
-casetest = pygameclass.Grillegraphic.grillegraphic(screen,50 ,50)
-casetest2 = pygameclass.Grillegraphic.grillegraphic(screen,700 ,50)
+
+torpilleur = pygame.image.load("images/torpilleur 2 cases.png")
+sous_marin = pygame.image.load("images/sous_marin 3 cases.png")
+porte_avion = pygame.image.load("images/porte-avion 4 cases.png")
+destroyer = pygame.image.load("images/destroyer 3 cases.png")
+croiseur = pygame.image.load("images/croiseur 5 cases.png")
+casetest = pygameclass.Grillegraphic.grillegraphic(screen, 50, 50)
+casetest2 = pygameclass.Grillegraphic.grillegraphic(screen, 700, 50)
 
 # Game loop.
 while True:
+    sprites = []
     screen.fill((4, 129, 220))
     casetest.drawgrid()
     casetest2.drawgrid()
+    sprites.append(screen.blit(torpilleur, (100, 500)))
+    sprites.append(screen.blit(sous_marin, (175, 500)))
+    sprites.append(screen.blit(porte_avion, (250, 500)))
+    sprites.append(screen.blit(destroyer, (325, 500)))
+    sprites.append(screen.blit(croiseur, (400, 500)))
+    pygame.display.flip()
     x, y = pygame.mouse.get_pos()
     gx, gy = casetest.coordScreenToGrid(x, y)
     gx2, gy2 = casetest2.coordScreenToGrid(x, y)
@@ -40,28 +53,29 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.MOUSEBUTTONUP:
-            if casetest.inGrid(x,y):
+            if sprites[0].collidepoint(pygame.mouse.get_pos()):
+                print("test1")
+            if casetest.inGrid(x, y):
                 p = 1 if p == 2 else 2
-                g = game.round(p,gx,gy)
+                g = game.round(p, gx, gy)
             print(x, y)
-            print('le 1 :::' , gx, gy)
-            print('le 2 :::' , gx2, gy2)
-            print('last coord' , casetest2.lastcoord)
+            print('le 1 :::', gx, gy)
+            print('le 2 :::', gx2, gy2)
+            print('last coord', casetest2.lastcoord)
 
     if casetest.inGrid(x,y):
         casetest.gridover(gx, gy)
         
     else:
-        casetest.gridout(casetest.lastcoord[0],casetest.lastcoord[1])
+        casetest.gridout(casetest.lastcoord[0], casetest.lastcoord[1])
 
     if casetest2.inGrid(x,y):
         casetest2.gridover(gx2, gy2)
     else:
-        casetest2.gridout(casetest2.lastcoord[0],casetest2.lastcoord[1])
+        casetest2.gridout(casetest2.lastcoord[0], casetest2.lastcoord[1])
 
     # Update.
 
     # Draw.
 
-    pygame.display.flip()
     fpsClock.tick(fps)
