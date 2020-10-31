c6a39d057d70ca74b8c37e3a9258094bbf0508a8
LeDevin <stenis34@gmail.com>
2020-10-31 12:05:00 +0100

    more graphic around boat

--- pygameclass/boatgraphic.py
+++ pygameclass/boatgraphic.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Boatgraphic:
     isselected = False
 
@@ -8,12 +10,15 @@ class Boatgraphic:
         self.initialx = x
         self.initialy = y
 
+    def addSquare(self, square):
+        self.square = square
+
     def coord(self):
         return [self.x, self.y]
 
-    def newcoord(self, coord):
-        self.x = coord[0]
-        self.y = coord[1]
+    def newcoord(self, coord, orientaion):
+        self.x = coord[0] - (self.square[2]/2 if orientaion else 0)
+        self.y = coord[1] - (self.square[3]/2 if not orientaion else 0)
 
     def reinitializecoord(self):
         self.x = self.initialx
--- testpygame.py
+++ testpygame.py
@@ -3,7 +3,7 @@ import sys
 import pygameclass.casegraphic
 import pygameclass.Grillegraphic
 import Partie
-import pygameclass.boatgraphic
+import pygameclass.boatgraphic as boatGraphic
 
 import math
 from pygame.locals import *
@@ -26,32 +26,31 @@ g = True
 # Variables
 
 
-torpilleur = pygameclass.boatgraphic.Boatgraphic(100, 500, pygame.image.load("images/torpilleur 2 cases.png"))
-sous_marin = pygameclass.boatgraphic.Boatgraphic(175, 500, pygame.image.load("images/sous_marin 3 cases.png"))
-porte_avion = pygameclass.boatgraphic.Boatgraphic(250, 500, pygame.image.load("images/porte-avion 4 cases.png"))
-destroyer = pygameclass.boatgraphic.Boatgraphic(325, 500, pygame.image.load("images/destroyer 3 cases.png"))
-croiseur = pygameclass.boatgraphic.Boatgraphic(400, 500, pygame.image.load("images/croiseur 5 cases.png"))
+torpilleur = boatGraphic.Boatgraphic(100, 550, pygame.image.load("images/torpilleur 2 cases.png"))
+sous_marin = boatGraphic.Boatgraphic(175, 500, pygame.image.load("images/sous_marin 3 cases.png"))
+destroyer = boatGraphic.Boatgraphic(250, 500, pygame.image.load("images/destroyer 3 cases.png"))
+porte_avion = boatGraphic.Boatgraphic(325, 500, pygame.image.load("images/porte-avion 4 cases.png"))
+croiseur = boatGraphic.Boatgraphic(400, 500, pygame.image.load("images/croiseur 5 cases.png"))
 casetest = pygameclass.Grillegraphic.grillegraphic(screen, 50, 50)
 casetest2 = pygameclass.Grillegraphic.grillegraphic(screen, 700, 50)
 team = []
 team.append(torpilleur)
 team.append(sous_marin)
-team.append(porte_avion)
 team.append(destroyer)
+team.append(porte_avion)
 team.append(croiseur)
 numberselected = -1
-
+orientation = True
 # Game loop.
 while True:
     sprites = []
     screen.fill((4, 129, 220))
+    buttunStart = pygame.draw.rect(screen, (255,255,255), (560,50,40,40))
     casetest.drawgrid()
     casetest2.drawgrid()
-    sprites.append(screen.blit(torpilleur.sprite, torpilleur.coord()))
-    sprites.append(screen.blit(sous_marin.sprite, sous_marin.coord()))
-    sprites.append(screen.blit(porte_avion.sprite, porte_avion.coord()))
-    sprites.append(screen.blit(destroyer.sprite, destroyer.coord()))
-    sprites.append(screen.blit(croiseur.sprite, croiseur.coord()))
+
+    for boat in team:
+        boat.addSquare(screen.blit(boat.sprite, boat.coord()))
     pygame.display.flip()
     x, y = pygame.mouse.get_pos()
     gx, gy = casetest.coordScreenToGrid(x, y)
@@ -61,34 +60,54 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-
-        if event.type == pygame.MOUSEBUTTONUP:
-            if pygameclass.boatgraphic.Boatgraphic.isselected:
-                selected.reinitializecoord()
-                pygameclass.boatgraphic.Boatgraphic.isselected = False
-
-            for j in range(5):
-                if numberselected == j:
-                    numberselected = -1
-                    print("test")
-                    continue
-                if sprites[j].collidepoint(pygame.mouse.get_pos()):
-                    selected = team[j]
-                    if pygameclass.boatgraphic.Boatgraphic.isselected:
-                        numberselected = j
-                    pygameclass.boatgraphic.Boatgraphic.isselected = True
-                    print("bateau :", j)
+    # all click event
+        # right click event
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+            print(90 if orientation else -90)
+            if boatGraphic.Boatgraphic.isselected:
+                orientation = False if orientation else True
+                selected.sprite = pygame.transform.rotate(selected.sprite,90 if orientation else -90)
+        # left click event
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            if buttunStart.collidepoint(pygame.mouse.get_pos()):
+                Partie.Partie.started = True
+                print("game start")
 
             if casetest.inGrid(x, y):
-                p = 1 if p == 2 else 2
-                g = game.round(p, gx, gy)
-            print(x, y)
-            print('le 1 :::', gx, gy)
-            print('le 2 :::', gx2, gy2)
-            print('last coord', casetest2.lastcoord)
+                if Partie.Partie.started:
+                    p = 1 if p == 2 else 2
+                    g = game.round(p, gx, gy)
+                else:
+                    if boatGraphic.Boatgraphic.isselected:
+                        size =  (2 if numberselected < 2 else 1)
+                        print("numberselected: ",numberselected, "\nsize: ",size)
+                        game.placeBoat(size + numberselected ,gy,gx,orientation,1)
+                        print("player 1 :")
+                        game.player1.showBoard()
+                print(x, y)
+                print('le 1 :::', gx, gy)
+                print('le 2 :::', gx2, gy2)
+                print('last coord', casetest2.lastcoord)
+
+            if boatGraphic.Boatgraphic.isselected:
+                selected.reinitializecoord()
+                boatGraphic.Boatgraphic.isselected = False
+                if not orientation:
+                    selected.sprite = pygame.transform.rotate(selected.sprite,-90)
+                selected.addSquare(screen.blit(selected.sprite, selected.coord()))
+                orientation = True
+
+            for j in range(5):
+                if team[j].square.collidepoint(pygame.mouse.get_pos()):
+                    selected = team[j]
+                    numberselected = j
+                    boatGraphic.Boatgraphic.isselected = True
+                    print("bateau :", numberselected)
+
+                    
 
     if pygameclass.boatgraphic.Boatgraphic.isselected:
-        selected.newcoord(pygame.mouse.get_pos())
+        selected.newcoord(pygame.mouse.get_pos(),orientation)
 
     if casetest.inGrid(x,y):
         casetest.gridover(gx, gy)
