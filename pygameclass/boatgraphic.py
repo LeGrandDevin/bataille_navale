3e83b3ee5973cfa9b738c934f2e866deaab131f7
LeDevin <stenis34@gmail.com>
2020-11-01 23:33:47 +0100

    a lot of thing

--- pygameclass/boatgraphic.py
+++ pygameclass/boatgraphic.py
@@ -9,6 +9,10 @@ class Boatgraphic:
         self.sprite = sprite
         self.initialx = x
         self.initialy = y
+        self.orientation = False
+        self.placed = False
+        self.gx = 0
+        self.gy = 0
 
     def addSquare(self, square):
         self.square = square
@@ -16,12 +20,27 @@ class Boatgraphic:
     def coord(self):
         return [self.x, self.y]
 
-    def newcoord(self, coord, orientaion):
-        self.x = coord[0] - (self.square[2]/2 if orientaion else 0)
-        self.y = coord[1] - (self.square[3]/2 if not orientaion else 0)
+    def newcoord(self, coord):
+        self.x = coord[0] - (self.square[2]/2 if not self.orientation else 0)
+        self.y = coord[1] - (self.square[3]/2 if self.orientation else 0)
 
     def reinitializecoord(self):
         self.x = self.initialx
         self.y = self.initialy
+        self.placed = False
 
-
+    def placeBoat(self,gridX,gridY,wichGrid):
+        add = 50 if wichGrid else 700
+        self.x = gridX * 50 + add  - (self.square[2]/2 - 25 if not self.orientation else 0)
+        self.y = gridY * 50 + 50 - (self.square[3]/2 - 25 if self.orientation else 0)
+        self.placed = True
+        self.gx = gridX
+        self.gy = gridY
+    def rotate(self):
+        if self.orientation:
+            self.sprite = pygame.transform.rotate(self.sprite,-90)
+            self.orientation = False
+        else:
+            self.sprite = pygame.transform.rotate(self.sprite,90)
+            self.orientation = True
+        print(self.orientation)
---
+++ pygameclass/gameGraphic.py
@@ -0,0 +1,86 @@
+import pygame
+from . import Grillegraphic
+from . import boatgraphic
+
+class gameGraphic:
+
+    def __init__(self,team,screen):
+        self.team = team
+        self.numberSelected = -1
+        self.screen = screen
+        self.grid1 = Grillegraphic.grillegraphic(screen, 50, 50)
+        self.grid2 = Grillegraphic.grillegraphic(screen, 700, 50)
+        self.inGrid = False
+        self.actualGrid = self.grid1
+        self.g = True
+
+
+    def updateBoatsSprite(self):
+        for boat in self.team:
+            boat.addSquare(self.screen.blit(boat.sprite, boat.coord()))
+
+    def getMousePos(self,coord):
+        self.mx = coord[0]
+        self.my = coord[1]
+
+    def wichGrid(self):
+        self.inGrid = True
+        if self.grid1.inGrid(self.mx,self.my):
+            self.actualGrid = self.grid1 
+            self.playerGrid = 1
+        elif self.grid2.inGrid(self.mx,self.my):
+            self.actualGrid = self.grid2
+            self.playerGrid = 2
+        else:
+            self.inGrid = False
+    
+    def coordScreenToGrid(self):
+        self.wichGrid()
+        self.gx , self.gy = self.actualGrid.coordScreenToGrid(self.mx,self.my)
+
+    def changeOrientaion(self):
+        if boatgraphic.Boatgraphic.isselected:
+            self.selected.rotate()
+
+    def placeBoat(self, p):
+        if self.inGrid and self.playerGrid == p:
+            self.selected.placeBoat(self.gx, self.gy, self.playerGrid == 1 )
+            boatgraphic.Boatgraphic.isselected = False
+            self.selected.addSquare(self.screen.blit(self.selected.sprite, self.selected.coord()))
+            return True
+        else:
+            self.selected.reinitializecoord()
+            boatgraphic.Boatgraphic.isselected = False
+            if self.selected.orientation:
+                self.selected.rotate()
+            self.selected.addSquare(self.screen.blit(self.selected.sprite, self.selected.coord()))
+            return False
+
+    def checkBoatClick(self):
+        for j in range(5):
+            if self.team[j].square.collidepoint(pygame.mouse.get_pos()):
+                self.selected = self.team[j]
+                self.numberselected = j
+                boatgraphic.Boatgraphic.isselected = True
+                self.selected.placed = False
+    
+    def mooveBoat(self):
+        self.selected.newcoord(pygame.mouse.get_pos())
+
+    def gridOver(self):
+        if self.inGrid:
+            self.actualGrid.gridover(self.gx, self.gy)
+        
+        else:
+            self.actualGrid.gridout(self.actualGrid.lastcoord[0], self.actualGrid.lastcoord[1])
+
+    def drawGrid(self):
+        self.grid1.drawgrid()
+        self.grid2.drawgrid()
+
+    def checkPlacedBoat(self,player):
+        for boat in self.team:
+            if boat.placed:
+                continue
+            return False
+        return True
